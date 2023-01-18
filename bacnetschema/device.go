b529b33a3407f9fb0becf39673251ca6e79618d3
NubeDev <aidan@enviroservices.com.au>
2023-01-18 14:18:04 +1100

    added auto mapping

--- bacnetschema/device.go
+++ bacnetschema/device.go
@@ -3,14 +3,16 @@ package bacnetschema
 import "github.com/NubeIO/lib-schema/schema"
 
 type DeviceSchema struct {
-	UUID           schema.UUID           `json:"uuid"`
-	Name           schema.Name           `json:"name"`
-	Description    schema.Description    `json:"description"`
-	Enable         schema.Enable         `json:"enable"`
-	DeviceObjectId schema.DeviceObjectId `json:"device_object_id"`
-	FastPollRate   schema.FastPollRate   `json:"fast_poll_rate"`
-	NormalPollRate schema.NormalPollRate `json:"normal_poll_rate"`
-	SlowPollRate   schema.SlowPollRate   `json:"slow_poll_rate"`
+	UUID                       schema.UUID                       `json:"uuid"`
+	Name                       schema.Name                       `json:"name"`
+	Description                schema.Description                `json:"description"`
+	Enable                     schema.Enable                     `json:"enable"`
+	DeviceObjectId             schema.DeviceObjectId             `json:"device_object_id"`
+	FastPollRate               schema.FastPollRate               `json:"fast_poll_rate"`
+	NormalPollRate             schema.NormalPollRate             `json:"normal_poll_rate"`
+	SlowPollRate               schema.SlowPollRate               `json:"slow_poll_rate"`
+	AutoMappingEnable          schema.AutoMappingEnable          `json:"auto_mapping_enable"`
+	AutoMappingFlowNetworkName schema.AutoMappingFlowNetworkName `json:"auto_mapping_flow_network_name"`
 }
 
 func GetDeviceSchema() *DeviceSchema {
--- edge28schema/device.go
+++ edge28schema/device.go
@@ -3,12 +3,14 @@ package edge28schema
 import "github.com/NubeIO/lib-schema/schema"
 
 type DeviceSchema struct {
-	UUID        schema.UUID        `json:"uuid"`
-	Name        schema.Name        `json:"name"`
-	Description schema.Description `json:"description"`
-	Enable      schema.Enable      `json:"enable"`
-	Host        schema.Host        `json:"host"`
-	Port        schema.Port        `json:"port"`
+	UUID                       schema.UUID                       `json:"uuid"`
+	Name                       schema.Name                       `json:"name"`
+	Description                schema.Description                `json:"description"`
+	Enable                     schema.Enable                     `json:"enable"`
+	Host                       schema.Host                       `json:"host"`
+	Port                       schema.Port                       `json:"port"`
+	AutoMappingEnable          schema.AutoMappingEnable          `json:"auto_mapping_enable"`
+	AutoMappingFlowNetworkName schema.AutoMappingFlowNetworkName `json:"auto_mapping_flow_network_name"`
 }
 
 func GetDeviceSchema() *DeviceSchema {
