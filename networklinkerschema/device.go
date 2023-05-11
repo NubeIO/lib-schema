58821c2fd0d077f82dae67e2d7bfd1203bf94850
Shiny380 <daniel.mckinnell43@gmail.com>
2023-05-12 08:55:06 +1000

    networklinker and lorawan options (#10)

--- networklinkerschema/device.go
+++ networklinkerschema/device.go
@@ -3,13 +3,13 @@ package networklinkerschema
 import "github.com/NubeIO/lib-schema/schema"
 
 type DeviceSchema struct {
-	Enable      schema.Enable `json:"enable"`
+	Enable      schema.Enable `json:"enable" deafult:"true"`
 	Name        schema.Name   `json:"name"`
 	AddressUUID struct {
-		Type    string   `json:"type" default:"string"`
-		Title   string   `json:"title" default:"address_uuid"`
-		Options []string `json:"enum" default:"[]"`
-		Help    string   `json:"help" default:"address_uuid"`
+		Type    string               `json:"type" default:"string"`
+		Title   string               `json:"title" default:"address_uuid"`
+		Options []schema.OptionOneOf `json:"oneOf"`
+		Help    string               `json:"help" default:"address_uuid"`
 	} `json:"address_uuid"`
 	AutoMappingEnable schema.AutoMappingEnableDefaultTrue `json:"auto_mapping_enable"`
 }
